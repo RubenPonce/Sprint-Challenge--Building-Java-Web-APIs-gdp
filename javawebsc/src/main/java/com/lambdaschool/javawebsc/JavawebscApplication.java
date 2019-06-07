4fe0c0e286809ec7e2332a8a100a2198b8b100ca
Ruben Ponce <ruben_ponce1@yahoo.com>
2019-06-07 08:38:46 -0700

    boiler plating the countrylist

---
+++ javawebsc/src/main/java/com/lambdaschool/javawebsc/JavawebscApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.javawebsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavawebscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavawebscApplication.class, args);
+    }
+
+}
